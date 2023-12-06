0dbd31a43b809373a6fa83d748dcad90db142767
Seojun Yoo <honeybeei@Seojuns-MacBook-Air.local>
2023-12-06 13:35:52 +0900

    dark-light theme button fixed

--- nextjs-starter-pagerouter-js/components/theme/ThemeBtn.jsx
+++ nextjs-starter-pagerouter-js/components/theme/ThemeBtn.jsx
@@ -1,11 +1,11 @@
-// "use client"
+"use client";
 
 import * as React from "react";
 
 import { useTheme } from "next-themes";
 
-import { Moon, Sun } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,13 +19,17 @@ export function ThemeBtn() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button
+          variant="outline"
+          size="icon"
+          className="flex justify-center items-center"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent>
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
