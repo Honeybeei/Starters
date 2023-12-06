14596be90d701fa2f6e174dfa843cdb03f98592c
Seojun Yoo <honeybeei@Seojuns-MacBook-Air.local>
2023-12-07 02:09:58 +0900

    layour added to app-router with ts

---
+++ nextjs-starter-approuter-ts/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className=" flex flex-row justify-center items-center bg-slate-500">
+      <h1>This is header</h1>
+    </header>
+  );
+}
